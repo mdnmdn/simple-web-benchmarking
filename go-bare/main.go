9b7c8d29e24d802697689565a00da2a49d3a9b3f
Marco De Nittis <marco.denittis@gmail.com>
2021-05-24 01:26:38 +0200

    go fmt

--- go-bare/main.go
+++ go-bare/main.go
@@ -1,15 +1,15 @@
 package main
 
-import( 
-  "fmt"
-  "net/http"
+import (
+	"fmt"
+	"net/http"
 )
 
 func handleRequest(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprint(res, "ok - go bare")
+	fmt.Fprint(res, "ok - go bare")
 }
 
 func main() {
-  http.HandleFunc("/", handleRequest)
-  http.ListenAndServe(":5000", nil)
+	http.HandleFunc("/", handleRequest)
+	http.ListenAndServe(":5000", nil)
 }
