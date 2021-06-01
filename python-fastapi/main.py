dd27afc5e842d8b8a07da2dc3a5b64f756859b14
Marco De Nittis <marco.denittis@gmail.com>
2021-06-01 13:54:42 +0200

    fastapi async init

--- python-fastapi/main.py
+++ python-fastapi/main.py
@@ -10,16 +10,17 @@ connection = None
 def randomKey():
   return "key_{}".format(randint(0,300))
 
+@app.on_event("startup")
+async def startup():
+  global connection
+  connection = await Connection.create(host="redis")
+
 @app.get("/", response_class=PlainTextResponse)
 def read_root():
-    return 'ok - fastapi'
+  return 'ok - fastapi'
 
 @app.get("/redis/incr", response_class=PlainTextResponse)
 async def redis_incr():
-  global connection
-  if connection == None:
-    connection = await Connection.create(host="redis")
-
   key = randomKey()
   val = await connection.incr(key)
   return 'fastapi: {} - {}'.format(key, val)
